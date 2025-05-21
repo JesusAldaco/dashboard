71fe2a2edf5803ff19799e9517954af45eef1f8f
JesusAldaco <jesus.ibanez@g-tech.io>
2025-05-21 11:16:36 -0600

    Tabla estudiante con despliegue de calificaciones

--- app/estudiantes/page.tsx
+++ app/estudiantes/page.tsx
@@ -5,9 +5,10 @@ export default function EstudiantesPage() {
         <main className="container mx-auto py-10">
             <h1 className="text-3xl font-bold mb-6">Sistema de estudiantes</h1>
             <p className="text-muted-foreground mb-6">
-                Haz clic en cualquier estudientes para ver sus detalles completos 
-                editarlo o eliminarlo. Para crear un nuevo estudiante, usa el botón
-                Nuevo estudiante
+                Haz clic en la matrícula de cualquier estudiante para ver sus detalles completos, 
+                editarlo o eliminarlo. Despliega las calificaciones con el icono de la flecha
+                a la izquierda de la matrícula. Para crear un nuevo estudiante, usa el botón 
+                Nuevo Estudiante.
             </p>
             <TablaEstudiantes />
         </main>
